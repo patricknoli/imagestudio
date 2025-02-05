be21b34e624ac0a61172219d718ccf7bc487658c
Patrick Oliveira <patrick@torors.com.br>
2025-02-05 03:19:02 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-black p-10'>
+        <h1 className='text-3xl text-yellow-800'>Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
