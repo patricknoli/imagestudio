bceed930c3b611bd52183570db0301ea8dc4227e
Patrick Oliveira <patrick@torors.com.br>
2025-02-07 04:12:32 -0300

    feat: add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css'
 import { Input } from './components/ui/input'
 import { Button } from './components/ui/button';
@@ -7,14 +7,23 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './comp
 import { Separator } from './components/ui/separator';
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from './components/ui/sheet';
 import { toast } from 'sonner';
+import { Canvas, controlsUtils, FabricImage, PencilBrush, Polygon } from 'fabric';
+
+export type classType = {
+  label: string; color: string
+}
 
 function App() {
+  const canvasRef = useRef(null);
+  const [canvas, setCanvas] = useState<Canvas>();
   const [image, setImage] = useState<File | null>(null);
-  const [tool, setTool] = useState<'brush' | 'polygon' | 'eraser'>('brush');
+  const [tool, setTool] = useState<'brush' | 'polygon'>();
   const [manageClasses, setManageClasses] = useState<boolean>(false);
-  const [classes, setClasses] = useState<{ label: string; color: string }[]>([]);
-  const [newClass, setNewClass] = useState<{ label: string; color: string }>({ label: '', color: '' });
+  const [classes, setClasses] = useState<classType[]>([]);
+  const [currentClass, setCurrentClass] = useState<classType>();
+  const [newClass, setNewClass] = useState<classType>({ label: '', color: '' });
   const [imageZoom, setImageZoom] = useState<number>(1);
+
 
   function addClass(newClass: { label: string; color: string }) {
     if (classes.find(item => item.color === newClass.color)) {
@@ -30,8 +39,93 @@ function App() {
   }
 
   useEffect(() => {
-    console.log(image);
-  }, [image])
+    if (canvasRef.current) {
+      const initCanvas = new Canvas(canvasRef.current, {
+        width: (window.innerWidth * 0.8),
+        height: window.innerWidth > 768 ? window.innerHeight : (window.innerHeight * 0.6),
+      });
+
+      initCanvas.backgroundColor = '#f0f0f0';
+      initCanvas.renderAll();
+
+      setCanvas(initCanvas);
+
+      return () => {
+        initCanvas.dispose();
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (image && canvas) {
+      let imgElement = document.createElement('img');
+      imgElement.src = URL.createObjectURL(image);
+      imgElement.onload = () => {
+        const img = new FabricImage(imgElement, {
+          scaleX: canvas.width / imgElement.width,
+          scaleY: canvas.width / imgElement.width,
+          selectable: false, // Disable dragging
+          evented: false // Disable events
+        });
+
+        if (imgElement.width > canvas.width) {
+          img.scaleToWidth(canvas.width);
+        }
+
+        canvas.add(img);
+        canvas.centerObject(img);
+      }
+    }
+  }, [image]);
+
+  function addPolygon() {
+    console.log('polygon');
+    if (canvas) {
+      const points = [
+        { x: 50, y: 0 },
+        { x: 100, y: 38 },
+        { x: 81, y: 100 },
+        { x: 19, y: 100 },
+        { x: 0, y: 38 }
+      ]
+
+      const poly = new Polygon(points, {
+        width: 200,
+        fill: currentClass ? currentClass.color : 'rgba(55, 155, 255, 0.5)',
+        scaleX: 5,
+        scaleY: 5
+      });
+      canvas.add(poly);
+      canvas.centerObject(poly);
+
+      let editing = false;
+      poly.on('mousedblclick', () => {
+        editing = !editing;
+        if (editing) {
+          poly.cornerStyle = 'circle';
+          poly.cornerColor = 'rgba(0,0,255,0.5)';
+          poly.hasBorders = false;
+          poly.controls = controlsUtils.createPolyControls(poly);
+        } else {
+          poly.cornerColor = 'blue';
+          poly.cornerStyle = 'rect';
+          poly.hasBorders = true;
+          poly.controls = controlsUtils.createObjectDefaultControls();
+        }
+        poly.setCoords();
+        canvas.requestRenderAll();
+      });
+    }
+  }
+
+  function brushMode() {
+    if (canvas) {
+      canvas.isDrawingMode = !canvas.isDrawingMode;
+      canvas.freeDrawingBrush = new PencilBrush(canvas);
+      canvas.freeDrawingBrush.width = 15;
+      canvas.freeDrawingBrush.color = currentClass ? currentClass.color : 'black';
+    }
+  }
 
   return (
     <>
@@ -44,127 +138,143 @@ function App() {
             onChange={(e) => setImage(e.target.files ? e.target.files[0] : null)} />
         </div>
 
-        {image && (
-          <>
-            <div className='preview relative mt-4 overflow-scroll md:overflow-visible'>
-              <div className='tools hidden md:flex absolute top-4 left-[-52px] flex-col gap-2 p-2 bg-gray-300 rounded'>
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Button variant="default" className={`${tool == "brush" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
-                        size="icon" onClick={() => setTool('brush')}>
-                        <Brush />
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <span className='text-white p-1 rounded bg-black/50 mb-2'>Brush annotation</span>
-                    </TooltipContent>
-                  </Tooltip>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Button variant="default" className={`${tool == "polygon" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
-                        size="icon" onClick={() => setTool('polygon')}>
-                        <SquareDashed />
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <span className='text-white p-1 rounded bg-black/50 mb-2'>Polygon annotation</span>
-                    </TooltipContent>
-                  </Tooltip>
+        <div className='preview relative mt-4 overflow-scroll md:overflow-visible md:pb-20'>
+          <div className='tools hidden md:flex absolute top-4 left-[-52px] flex-col gap-2 p-2 bg-gray-300 rounded'>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button variant="default" className={`${tool == "brush" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
+                    size="icon" onClick={() => { brushMode(); setTool('brush') }}>
+                    <Brush />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span className='text-white p-1 rounded bg-black/50 mb-2'>Brush annotation</span>
+                </TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button variant="default" className={`${tool == "polygon" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
+                    size="icon" onClick={() => { addPolygon(); setTool('polygon') }}>
+                    <SquareDashed />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span className='text-white p-1 rounded bg-black/50 mb-2'>Polygon annotation</span>
+                </TooltipContent>
+              </Tooltip>
 
-                  <Separator className='my-1 bg-black' />
+              <Separator className='my-1 bg-black' />
 
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Button variant="default" className={`${tool == "eraser" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
-                        size="icon" onClick={() => setTool('eraser')}>
-                        <Eraser />
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <span className='text-white p-1 rounded bg-black/50 mb-2'>Eraser</span>
-                    </TooltipContent>
-                  </Tooltip>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Button variant="default" className='bg-zinc-600 text-white' size="icon">
-                        <Undo />
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <span className='text-white p-1 rounded bg-black/50 mb-2'>Undo last edition</span>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              </div>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button variant="default" className={`bg-zinc-600 text-white`}
+                    size="icon" onClick={() => { }}>
+                    <Eraser />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span className='text-white p-1 rounded bg-black/50 mb-2'>Eraser</span>
+                </TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button variant="default" className='bg-zinc-600 text-white' size="icon">
+                    <Undo />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span className='text-white p-1 rounded bg-black/50 mb-2'>Undo last edition</span>
+                </TooltipContent>
+              </Tooltip>
 
-              <img src={URL.createObjectURL(image)} alt="" style={{
-                transform: `scale(${imageZoom})`,
-              }} />
-            </div>
-            <div className='tools md:hidden flex gap-2 p-2 bg-gray-300 rounded'>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Button variant="default" className={`${tool == "brush" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
-                      size="icon" onClick={() => setTool('brush')}>
-                      <Brush />
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <span className='text-white p-1 rounded bg-black/50 mb-2'>Brush annotation</span>
-                  </TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Button variant="default" className={`${tool == "polygon" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
-                      size="icon" onClick={() => setTool('polygon')}>
-                      <SquareDashed />
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <span className='text-white p-1 rounded bg-black/50 mb-2'>Polygon annotation</span>
-                  </TooltipContent>
-                </Tooltip>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button size="icon" className='bg-transparent' onClick={() => setManageClasses(true)}>
+                    <span className='w-4 h-4 rounded-full' style={{ backgroundColor: currentClass?.color }}></span>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span className='text-white p-1 rounded bg-black/50 mb-2'>Current class</span>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
 
-                <Separator orientation='vertical' className='my-1 bg-black h-[30px]' />
+          <canvas id="canvas" ref={canvasRef} />
+        </div>
+        <div className='tools md:hidden flex gap-2 p-2 bg-gray-300 rounded'>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button variant="default" className={`${tool == "brush" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
+                  size="icon" onClick={() => setTool('brush')}>
+                  <Brush />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span className='text-white p-1 rounded bg-black/50 mb-2'>Brush annotation</span>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button variant="default" className={`${tool == "polygon" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
+                  size="icon" onClick={() => { addPolygon(); setTool('polygon') }}>
+                  <SquareDashed />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span className='text-white p-1 rounded bg-black/50 mb-2'>Polygon annotation</span>
+              </TooltipContent>
+            </Tooltip>
 
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Button variant="default" className={`${tool == "eraser" ? 'bg-sky-600' : 'bg-zinc-600'}  text-white`}
-                      size="icon" onClick={() => setTool('eraser')}>
-                      <Eraser />
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <span className='text-white p-1 rounded bg-black/50 mb-2'>Eraser</span>
-                  </TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Button variant="default" className='bg-zinc-600 text-white' size="icon">
-                      <Undo />
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <span className='text-white p-1 rounded bg-black/50 mb-2'>Undo last edition</span>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
+            <Separator orientation='vertical' className='my-1 bg-black h-[30px]' />
 
-              <Separator orientation='vertical' className='my-1 bg-black h-[30px]' />
+            <Tooltip>
+              <TooltipTrigger>
+                <Button variant="default" className={`bg-zinc-600 text-white`}
+                  size="icon" onClick={() => { }}>
+                  <Eraser />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span className='text-white p-1 rounded bg-black/50 mb-2'>Eraser</span>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button variant="default" className='bg-zinc-600 text-white' size="icon">
+                  <Undo />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span className='text-white p-1 rounded bg-black/50 mb-2'>Undo last edition</span>
+              </TooltipContent>
+            </Tooltip>
 
-              <Button variant="default" className='bg-zinc-600 text-white' size="icon"
-                onClick={() => setImageZoom(imageZoom + 0.1)}>
-                <ZoomIn />
-              </Button>
-              <Button variant="default" className='bg-zinc-600 text-white' size="icon"
-                onClick={() => setImageZoom(imageZoom - 0.1)}>
-                <ZoomOut />
-              </Button>
-            </div>
-          </>
-        )}
+            <Tooltip>
+              <TooltipTrigger>
+                <Button size="icon" className='bg-transparent' onClick={() => setManageClasses(true)}>
+                  <span className='w-4 h-4 rounded-full' style={{ backgroundColor: currentClass?.color }}></span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span className='text-white p-1 rounded bg-black/50 mb-2'>Current class</span>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <Separator orientation='vertical' className='my-1 bg-black h-[30px]' />
+
+          <Button variant="default" className='bg-zinc-600 text-white' size="icon"
+            onClick={() => setImageZoom(imageZoom + 0.1)}>
+            <ZoomIn />
+          </Button>
+          <Button variant="default" className='bg-zinc-600 text-white' size="icon"
+            onClick={() => setImageZoom(imageZoom - 0.1)}>
+            <ZoomOut />
+          </Button>
+        </div>
       </div>
 
       <Sheet open={manageClasses} onOpenChange={() => setManageClasses(false)}>
@@ -190,10 +300,10 @@ function App() {
                 {classes.map((item, index) => (
                   <li key={index} className='flex gap-4 items-center'>
                     <span className='w-4 h-4 rounded-full' style={{ backgroundColor: item.color }}></span>
-                    <span>{item.label}</span>
+                    <span>{item.label} {currentClass == item && "(current)"}</span>
                     <div className='flex gap-2 ml-auto'>
-                      <Button variant='default' size="icon" className='bg-black text-white w-6 h-6'><Pencil /></Button>
-                      <Button variant='default' size="icon" className='bg-black text-white w-6 h-6' onClick={() => removeClass(index)}><Trash /></Button>
+                      <Button variant='default' className='bg-black text-white' onClick={() => setCurrentClass(item)}>Select</Button>
+                      <Button variant='default' size="icon" className='bg-black text-white' onClick={() => removeClass(index)}><Trash /></Button>
                     </div>
                   </li>
                 ))}
@@ -203,7 +313,7 @@ function App() {
         </SheetContent>
       </Sheet>
 
-      <div className='footer fixed bottom-0 left-0 w-full p-4 bg-white shadow-lg'>
+      <div className='footer fixed bottom-0 left-0 w-full p-2 md:p-4 bg-white shadow-lg'>
         <div className='container mx-auto flex gap-4 items-center'>
           <Button variant='default' className='bg-black text-white' onClick={() => setManageClasses(true)}>Manage classes</Button>
           <Button variant='default' className='bg-black text-white'>Save annotations</Button>
